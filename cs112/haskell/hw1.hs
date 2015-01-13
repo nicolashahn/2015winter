b06b52d2f3677aaea8b2b50970661397135ac6f0
nicolashahn <nhahn@ucsc.edu>
2015-01-12 16:59:59 -0800

    cs140 textbook, cs112hw1

--- cs112/haskell/hw1.hs
+++ cs112/haskell/hw1.hs
@@ -19,6 +19,14 @@ bibliography_rec [] = ""
 bibliography_rec [(auth,book,date)] = citeBook (head [(auth,book,date)])
 bibliography_rec (x:xs) = citeBook x ++ "\n" ++ bibliography_rec (xs)
 
---bibliography_fold :: [(String, String, Int)] -> String
---bibliography_fold [] = ""
---bibliography_fold
+--foldl takes 3 arguments:
+	--what to do with the current element
+	--starting element
+	--list to deal with
+--inner function in foldl takes 2:
+	--the accumulator (result of all previous foldl's)
+	--the current element
+bibliography_fold :: [(String, String, Int)] -> String
+bibliography_fold [] = ""
+bibliography_fold xs = foldl(\acc x -> acc ++ citeBook x ++ "\n") "" xs
+
